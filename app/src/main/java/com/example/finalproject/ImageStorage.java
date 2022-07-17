a8d2b2c283631f491825dd7fc90558a34732c33e
ccreaco <ccreao@outlook.com>
2022-07-17 19:48:34 -0400

    final_project

---
+++ app/src/main/java/com/example/finalproject/ImageStorage.java
@@ -0,0 +1,111 @@
+package com.example.finalproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+
+public class ImageStorage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar tBar;
+    private DrawerLayout drawer;
+    private NavigationView navView;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_storage);
+
+        tBar = findViewById(R.id.toolbar);
+        setSupportActionBar(tBar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+                drawer, tBar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        tBar.setNavigationIcon(R.drawable.icons8_nasa_48);
+
+        navView = findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String message = null;
+
+        switch(item.getItemId()) {
+
+            case R.id.savedPicturesList:
+                message = "You clicked item 1!";
+                break;
+            case R.id.item2:
+                message = "You clicked item 2!";
+                break;
+        }
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_menu, menu);
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        String message = null;
+
+        switch(item.getItemId())
+        {
+            case R.id.home:
+                Intent home = new Intent(this, MainActivity.class);
+                message = "Home";
+                startActivity(home);
+                break;
+            case R.id.about:
+                Intent nextPage = new Intent(this, AboutNASA.class);
+                message = "About NASA";
+                startActivity(nextPage);
+                break;
+            case R.id.image:
+                Intent images = new Intent(this, Images.class);
+                message = "Images";
+                startActivity(images);
+                break;
+            case R.id.exit:
+                message = "Goodbye";
+                finishAffinity();
+                break;
+
+        }
+
+        drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        Toast.makeText(this,  message, Toast.LENGTH_SHORT).show();
+        return false;
+    }
+}
